fe64534cbc521c8b3c4e68476cffbf6173236b44
zxt <552355101@qq.com>
2020-11-20 16:03:38 +0800

    首页1

---
+++ demo/src/components/Home.vue
@@ -0,0 +1,32 @@
+<template>
+    <div class="home">
+        home
+    </div>
+</template>
+
+<script>
+export default {
+  name: '',
+  props: {
+      
+  },
+  data() {
+    return {
+      
+    }
+  },
+  created(){
+      //初始化某些属性值
+  },
+  mounted() {
+     //通常是初始化页面完成后，再对html的dom节点进行一些需要的操作
+  },
+  methods: {
+
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
